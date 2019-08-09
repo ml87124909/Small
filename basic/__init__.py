4f7c33e83b3eb31f2d4e608b524f2935a08ae3c6
mn3711698 <173782910@qq.com>
2019-08-09 22:14:55 +0800

    更新模块

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,11 +1,10 @@
 ## -*- coding: utf-8 -*-
-import platform,sys
-
-if platform.system() == "FreeBSD":
-    from . import publicf  as public
-elif platform.system()=="Linux":
-    from . import publicw  as public
-
-else:
-    print("There is no reactor can be used.")
-    sys.exit(1)
+# import platform,sys
+#
+# if platform.system() == "FreeBSD":
+#     from . import publicf  as public
+# elif platform.system()=="Linux":
+#     from . import publicw  as public
+# else:
+#     print("There is no reactor can be used.")
+#     sys.exit(1)
