57344c6964161d8d893c3174780f4155f5bb1ab0
mn3711698 <173782910@qq.com>
2019-10-22 19:18:29 +0800

    增加开发部署步骤演示文档

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,13 +1,13 @@
 ## -*- coding: utf-8 -*-
 import platform,sys
 
-if platform.system() == "FreeBSD":
-    from . import publicf  as public
-elif platform.system()=="Linux":
-    try:
-        from . import publicu  as public#ubuntu  18.04 64
-    except:
-        from . import publicc  as public#centos 7.3 64
-else:
-    print("must be python3.6,")
-    sys.exit(1)
+# if platform.system() == "FreeBSD":
+#     from . import publicf  as public
+# elif platform.system()=="Linux":
+#     try:
+#         from . import publicu  as public#ubuntu  18.04 64
+#     except:
+#         from . import publicc  as public#centos 7.3 64
+# else:
+#     print("must be python3.6,")
+#     sys.exit(1)
