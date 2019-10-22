195ebd1dce146ccd432a40c607443269765726ea
mn3711698 <173782910@qq.com>
2019-10-22 18:00:47 +0800

    增加安装或配置失败的处理提醒

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,10 +1,13 @@
 ## -*- coding: utf-8 -*-
-# import platform,sys
-#
-# if platform.system() == "FreeBSD":
-#     from . import publicf  as public
-# elif platform.system()=="Linux":
-#     from . import publicw  as public
-# else:
-#     print("There is no reactor can be used.")
-#     sys.exit(1)
+import platform,sys
+
+if platform.system() == "FreeBSD":
+    from . import publicf  as public
+elif platform.system()=="Linux":
+    try:
+        from . import publicu  as public#ubuntu  18.04 64
+    except:
+        from . import publicc  as public#centos 7.3 64
+else:
+    print("must be python3.6,")
+    sys.exit(1)
