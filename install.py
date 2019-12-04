7cd9afea63669149c8448804423881b3532b4ddd
mn3711698 <173782910@qq.com>
2019-12-04 13:14:43 +0800

    修改安装地址

--- install.py
+++ install.py
@@ -861,8 +861,8 @@ def start():
 
 
 if __name__ == '__main__':
-    app.run(port=5001)
-    #app.run(host='0.0.0.0', port=5000,debug=True)
+    #app.run(port=5001)
+    app.run(host='0.0.0.0', port=5000,debug=True)
 
 
 
